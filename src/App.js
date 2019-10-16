47233afba8f9c7e14bf6f6842f94a43cf8d55e86
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-16 13:45:42 -0400

    cjr-change

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <main>
       <h1>Create React App + Go API</h1>
+      <h2>Is this working? Created my Branch!</h2>
       <h2>
         Deployed with{' '}
         <a
